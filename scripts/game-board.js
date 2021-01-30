b1803b99b06797bd3d329b2e24de2e870027b88d
ShanMehr <ishanmeher25@gmail.com>
2021-01-29 20:49:14 -0800

    Create Board

--- scripts/game-board.js
+++ scripts/game-board.js
@@ -1,31 +1,32 @@
-
-
-
-
-
 const GameBoard = (function(){
     let board = [['X','X','X'],['O','X','O'],['X','O','X']];
     let recordMove=(playerMark)=>{
 
     }
     let makeBoard=()=>{
-        const boardDisplay= document.getElementById("game-container");
+        const window= document.getElementById("container");
+        const boardDisplay= document.createElement("div");
+        boardDisplay.setAttribute("id","game-input-container");
+        window.appendChild(boardDisplay);
+        const container = document.createElement("div");        
+        container.setAttribute("id","game-container");
+        boardDisplay.appendChild(container);
         for (i = 0; i < 3; i++) {
             const row = document.createElement("div");
-            row.setAttribute("id",`${i}`);
-            row.innerHTML="row";
-            console.log(i);
-            
-            boardDisplay.appendChild(row);
+            row.setAttribute("id",`row-${i}`);
+            row.className="board-row";
+                       
+            container.appendChild(row);
             
             for(j=0;j<3;j++){
                 // Append columns to the rows
                 const col = document.createElement("div");
-                col.innerHTML="row";
-                col.className="cell";
+                col.innerHTML=`row-${i}-col${j}`;
+                
+                col.className="board-col";
                 col.setAttribute("id",`${j}`);
                 row.appendChild(col);     
-                console.log(j)
+                
             }
           }
     }
@@ -45,8 +46,7 @@ const Player=()=>{
    const makeMove=()=>{
     // make a move 
    }
-   const getName=()=>{
-       return this.name;
+   const getName=()=>{col.innerHTML=`X`;
    }
    const setName=(name)=>{
        this.name=name;
@@ -55,28 +55,28 @@ const Player=()=>{
    return {makeMove,getName,setName}
 };
 
-const TicTacToe=((gameBoard)=>{
+const TicTacToe=(function(){
     let player1WinCount=0;
     let player2WinCount=0;
-    let board= gameBoard;
-    let startGame=()=>{
-        // Takes a board as argument
-        // makes a board
-        //loadStartScreen() 
-        makeBoard();
+    const board = new GameBoard();
+    let startGame=()=>{       
+        board.makeBoard();
+
     }
     let loadStartScreen=()=>{
         //makes the start screen 
-        const container = document.getElementById("game-container")
-    }
+        const container = document.getElementById("game-input-container");
+    }   
     return{startGame}
-})();
+});
 
 
 
-
+/*
 const tictactoeBoard = new GameBoard();
 
 tictactoeBoard.makeBoard();
-
-console.log("hello world")
+*/
+let game = new TicTacToe();
+game.startGame();
+console.log("hello world");
